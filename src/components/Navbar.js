757e4d9d4670223b97c7b11d4cc5765d18ee7f6b
sambeck87 <sandro_mipb@hotmail.com>
2023-01-30 08:58:27 -0600

    Save img on img folder

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import '../styles/navbar.css';
-import logo from './planet.png';
+import logo from '../images/planet.png';
 
 function Navbar() {
   return (
