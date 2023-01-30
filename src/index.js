234b6226abd267207eff1f732e77cdd3b78221c6
Belay Birhanu Gibina <2belamit@gmail.com>
2023-01-30 20:32:39 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,10 +9,10 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
 );
