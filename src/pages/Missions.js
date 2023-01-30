e2927ba74e81b04e0940d0b1b40e727f6d041882
belaymit <2belamit@gmail.com>
2023-01-30 21:38:16 +0300

    created basic structure for Mission

--- src/pages/Missions.js
+++ src/pages/Missions.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import Mission from '../components/mission/Mission';
+import '../styles/mission.scss';
 
 const Missions = () => (
-  <div>Missions</div>
+  <section className="mission-section">
+    <div className="container">
+      <ul className="mission-container">
+        <li className="mission-list">
+          <span className="mission-info">Mission</span>
+          <span className="mission-info">Description</span>
+          <span className="mission-info">Status</span>
+        </li>
+        <Mission />
+      </ul>
+    </div>
+  </section>
 );
 
 export default Missions;
