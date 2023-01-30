b491fe8ab6c7f3be46b5cd00cbf66039d07b17b9
belaymit <2belamit@gmail.com>
2023-01-30 22:34:52 +0300

    added a fucntionality to fetch data from store

--- src/pages/Missions.js
+++ src/pages/Missions.js
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Mission from '../components/mission/Mission';
+import { getAllMissions } from '../redux/mission/mission';
 import '../styles/mission.scss';
 
-const Missions = () => (
-  <section className="mission-section">
-    <div className="container">
-      <ul className="mission-container">
-        <li className="mission-list">
-          <span className="mission-info">Mission</span>
-          <span className="mission-info">Description</span>
-          <span className="mission-info">Status</span>
-        </li>
-        <Mission />
-      </ul>
-    </div>
-  </section>
-);
+let flag = false;
+const Missions = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (!flag) {
+      dispatch(getAllMissions());
+      flag = true;
+    }
+  }, [dispatch]);
+
+  const missions = useSelector((state) => state.missionsReducer.missions);
+  return (
+    <section className="mission-section">
+      <div className="container">
+        <ul className="mission-container">
+          <li className="mission-list">
+            <span className="mission-info">Mission</span>
+            <span className="mission-info">Description</span>
+            <span className="mission-info">Status</span>
+          </li>
+          {missions?.map((mission) => (
+            <Mission key={mission.mission_id} mission={mission} />
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
+};
 
 export default Missions;
