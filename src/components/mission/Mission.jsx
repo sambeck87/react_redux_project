48948c321e327c1fda2e8e5e6680a32ff502ff83
belaymit <2belamit@gmail.com>
2023-01-31 11:34:26 +0300

    rendered fetched mission

--- src/components/mission/Mission.jsx
+++ src/components/mission/Mission.jsx
@@ -1,26 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { cancelMission, reserveMission } from '../../redux/mission/mission';
 
-const Mission = ({ mission }) => (
-  <div className="mission">
-    <h3 className="mission-title">{mission.mission_name}</h3>
-    <p className="mission-description">{mission.description}</p>
-    <div className="mission-status">
-      <button
-        type="button"
-        className="active-member"
-      >
-        Active Member
-      </button>
-      <button
-        type="button"
-        className="leave-button"
-      >
-        Leave Mission
-      </button>
+const Mission = ({ mission }) => {
+  const dispatch = useDispatch();
+  const handleReservation = () => {
+    if (mission.status) {
+      dispatch(cancelMission(mission.mission_id));
+    } else {
+      dispatch(reserveMission(mission.mission_id));
+    }
+  };
+  return (
+    <div className="mission">
+      <h3 className="mission-title">{mission.mission_name}</h3>
+      <p className="mission-description">{mission.description}</p>
+      <div className="mission-status">
+        <div className="mission-status-btn">
+          <button
+            type="button"
+            className={mission.status ? 'active-member' : 'not-member'}
+          >
+            {mission.status ? 'Active Member' : 'Not a Member'}
+          </button>
+        </div>
+        <button
+          type="button"
+          onClick={handleReservation}
+          className={mission.status ? 'leave-mission' : 'join-mission'}
+        >
+          {mission.status ? 'Leave Mission' : 'Join Mission'}
+        </button>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 Mission.propTypes = {
   mission: PropTypes.shape({
