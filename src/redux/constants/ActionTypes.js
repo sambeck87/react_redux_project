409043d82809e7f2206ee82442ce551c173c4575
belaymit <2belamit@gmail.com>
2023-01-31 18:06:10 +0300

    added url to constants

--- src/redux/constants/ActionTypes.js
+++ src/redux/constants/ActionTypes.js
@@ -4,3 +4,7 @@ export const MISSIONS_SUCCESS = 'MISSIONS_SUCCESS';
 export const MISSIONS_FAILURE = 'MISSIONS_FAILURE';
 export const JOIN_MISSION = 'JOIN_MISSION';
 export const LEAVE_MISSION = 'LEAVE_MISSION';
+
+// url
+
+export const URL = 'https://api.spacexdata.com/v3/missions';
