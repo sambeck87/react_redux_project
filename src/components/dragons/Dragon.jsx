322827eef3e2e0fbb4cdc1bf71dae9a5bfff3380
Vgonma <victorgmassimi.microverse@gmail.com>
2023-01-31 12:48:27 -0600

    Created actions to add and remove reservations

--- src/components/dragons/Dragon.jsx
+++ src/components/dragons/Dragon.jsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+// Component imports
+import { reserve } from '../../redux/dragons/Dragons';
 
-function Dragon({ name, type, image, reserved }) {
+function Dragon({
+  id,
+  name,
+  type,
+  image,
+  reserved,
+}) {
+  const [dragonId] = useState(id);
+  const dispatch = useDispatch();
+  function toggleReserve() {
+    dispatch(reserve(dragonId));
+  }
+
   return (
     <div className="single-dragon-container">
       <div className="dragon-image-container">
@@ -16,7 +31,7 @@ function Dragon({ name, type, image, reserved }) {
             {reserved ? 'Reserved' : '' }
           </span>
         </p>
-        <button type="button" className="reserve btn">Reserve Dragon</button>
+        <button type="button" className="reserve btn" onClick={toggleReserve}>Reserve Dragon</button>
       </div>
 
     </div>
@@ -24,6 +39,7 @@ function Dragon({ name, type, image, reserved }) {
 }
 
 Dragon.propTypes = {
+  id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   type: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
--- src/redux/dragons/Dragons.js
+++ src/redux/dragons/Dragons.js
@@ -16,6 +16,7 @@ export const fetchDragons = createAsyncThunk(
         name: dragon.name,
         type: dragon.type,
         images: [...dragon.flickr_images],
+        reserved: false,
       }));
       return parsedData;
     }),
@@ -29,6 +30,16 @@ const dragonsSlice = createSlice({
     loading: false,
   },
   reducers: {
+    reserve: (state, action) => {
+      const reservedArr = state.dragons.map((dragon) => {
+        if (dragon.id !== action.payload) {
+          return dragon;
+        }
+        return { ...dragon, reserved: true };
+      });
+      return { ...state, dragons: reservedArr };
+    },
+
   },
   extraReducers: {
     [fetchDragons.fulfilled]: (state, action) => {
@@ -47,4 +58,6 @@ const dragonsSlice = createSlice({
   },
 });
 
+const { actions } = dragonsSlice;
+export const { reserve } = actions;
 export default dragonsSlice.reducer;
