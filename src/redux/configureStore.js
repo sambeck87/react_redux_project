b891d6bcd7da9641803d571a1c92c072c2103d84
belaymit <2belamit@gmail.com>
2023-01-31 18:05:33 +0300

    added missinReducer

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,11 +1,11 @@
 import { applyMiddleware, configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
-import missionsReducer from './mission/mission';
+import missionReducer from './mission/mission';
 
 const store = configureStore({
   reducer: {
-    missionsReducer,
+    missionReducer,
   },
 }, applyMiddleware(thunk, logger));
 
